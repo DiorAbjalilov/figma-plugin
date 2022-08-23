1e67fe1a517269874b7be39fbc77065a896dbefe
DiorAbjalillov <abjalilovdiyor@gmail.com>
2022-08-23 18:56:01 +0500

    edit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Alert, Button, Form } from "react-bootstrap";
 import { api } from "../App";
 import "./style.css";
 const Search = () => {
@@ -8,6 +8,7 @@ const Search = () => {
   const [folderOne, setFolderOne] = useState([]);
   const [category, setCategory] = useState([]);
   const [icons, setIcons] = useState([]);
+  const [alert, setAlert] = useState(false);
   const getFolder = async () => {
     try {
       const res = await axios.get(api + "folders");
@@ -39,44 +40,75 @@ const Search = () => {
       console.log(error);
     }
   };
+
+  // delete icon
+  const deleteIcon = async (path) => {
+    try {
+      const res = await axios.delete(api + "delete?path=" + path);
+      if (res.status == 200) {
+        setAlert(true);
+        setTimeout(() => {
+          setAlert(false);
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getFolder();
   }, []);
 
   return (
-    <div className="search">
-      <div className="searchInput">
-        <Form.Select onChange={(e) => getCategory(e.target.value)}>
-          <option>Default select</option>
-          {folder &&
-            folder.map((item, index) => {
-              return <option key={index}>{item}</option>;
-            })}
-        </Form.Select>
-        <Form.Select onChange={(e) => getIcons(folderOne, e.target.value)}>
-          <option>Default select</option>
-          {category &&
-            category.map((item, index) => {
-              return <option key={index}>{item}</option>;
-            })}
-        </Form.Select>
-        <div className="icons">
-          {icons.length === 0 ? (
-            <p>no item</p>
-          ) : (
-            icons.map((item, index) => {
-              return (
-                <div className="icon" key={index}>
-                  <img src={api + "static/" + item.path} />
-                  <p>{item.name}</p>
-                  <Button variant="danger">Delete</Button>
-                </div>
-              );
-            })
-          )}
+    <>
+      <div className="search">
+        <div className="searchInput">
+          <Form.Select onChange={(e) => getCategory(e.target.value)}>
+            <option>Default select</option>
+            {folder &&
+              folder.map((item, index) => {
+                return <option key={index}>{item}</option>;
+              })}
+          </Form.Select>
+          <Form.Select onChange={(e) => getIcons(folderOne, e.target.value)}>
+            <option>Default select</option>
+            {category &&
+              category.map((item, index) => {
+                return <option key={index}>{item}</option>;
+              })}
+          </Form.Select>
+          <div className="icons">
+            {icons.length === 0 ? (
+              <p>no item</p>
+            ) : (
+              icons.map((item, index) => {
+                return (
+                  <div className="icon" key={index}>
+                    <img src={api + "static/" + item.path} />
+                    <p>{item.name}</p>
+                    <Button
+                      variant="danger"
+                      onClick={() => deleteIcon(item.path)}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                );
+              })
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      {alert && (
+        <Alert
+          style={{ position: "absolute", right: "15px", top: "15px" }}
+          variant="success"
+        >
+          File is delete
+        </Alert>
+      )}
+    </>
   );
 };
 
